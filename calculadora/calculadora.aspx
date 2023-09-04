9dcd34209e2e4a68cb1ac954fc5566c905189e1c
eliseooca <eliseo.oca29@gmail.com>
2023-09-04 19:26:22 -0300

    Agregar archivos de proyecto.

---
+++ calculadora/calculadora.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="calculadora.aspx.cs" Inherits="calculadora.calculadora" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Calculadora Web</h2>
+            <asp:TextBox ID="TextBox1" runat="server" placeholder="Ingrese un Numero"></asp:TextBox>
+            <br />
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem Text="Sumar" Value="sumar"/>
+                <asp:ListItem Text="Restar" Value="restar"/>
+                <asp:ListItem Text="Multiplicar" Value="multiplicar"/>
+                <asp:ListItem Text="Dividir" Value="dividir"/>
+            </asp:DropDownList>
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server" placeholder="Ingrese el Segundo Numero"></asp:TextBox>
+        </div>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Calcular" OnClick="calculateButton_Click" />
+            <asp:Label ID="resultLabel" runat="server"  EnableViewState="false"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ calculadora/calculadora.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace calculadora
+{
+    public partial class calculadora : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void calculateButton_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(TextBox1.Text);
+            double num2 = Convert.ToDouble(TextBox2.Text);
+            string operacion = DropDownList1.SelectedValue;
+
+            double resultado = 0;
+
+            switch (operacion)
+            {
+                case "sumar":
+                    resultado = num1 + num2;
+                    break;
+
+                case "restar":
+                    resultado = num1 - num2;
+                    break;
+
+                case "multiplicar":
+                    resultado = num1 * num2;
+                    break;
+
+                case "dividir":
+                    if (num2 != 0)
+                    {
+                        resultado = num1 / num2;
+                    }
+                    else
+                    {
+                        resultLabel.Text = "No se puede dividir por cero";
+                        return;
+                    }
+                    break;
+            }
+            resultLabel.Text = $"Resultado Final: {resultado}";
+            
+
+        }
+
+    }
+}
